0f01286ceb34c4ce733c5f937a93b11662edf07b
Mal Curtis <mal@mal.co.nz>
2016-03-22 17:36:27 +1300

    Initial commit

---
+++ go/controller/controller.go
@@ -0,0 +1,74 @@
+package controller
+
+import "github.com/kidoman/embd"
+
+// Controller represents a fan in thermostat controller for multiple zones.
+type Controller struct {
+	Zones           map[string]*Zone `json:"zones"`
+	Active          bool             `json:"active"`
+	heaterPinNumber uint32
+	heaterPin       embd.DigitalPin
+	change          chan bool
+}
+
+// NewController returns an initialized controller on the supplied pin.
+func NewController(pin uint32) *Controller {
+	return &Controller{
+		Zones:           map[string]*Zone{},
+		heaterPinNumber: pin,
+		change:          make(chan bool),
+	}
+}
+
+// AddZone adds a zone and starts running it.
+func (controller *Controller) AddZone(zone *Zone) {
+	controller.Zones[zone.Name] = zone
+	zone.run(controller.change)
+}
+
+// Run starts the controller running and listening for zone changes.
+func (controller *Controller) Run() {
+	// Start GPIO and defer its closing to ensure cleanup.
+	embd.InitGPIO()
+	defer embd.CloseGPIO()
+	controller.initPins()
+
+	// Start a loop to check on the zones when a change is emitted.
+	for {
+		<-controller.change
+		anyActive := false
+		for _, zone := range controller.Zones {
+			if zone.Active {
+				anyActive = true
+				if !controller.Active {
+					controller.Active = true
+					controller.SetHeaterState()
+				}
+				break
+			}
+		}
+		if !anyActive && controller.Active {
+			controller.Active = false
+			controller.SetHeaterState()
+		}
+	}
+}
+
+// SetHeaterState will adjust the heater pin to the correct high / low value.
+func (controller *Controller) SetHeaterState() {
+	if controller.Active {
+		controller.heaterPin.Write(embd.High)
+	} else {
+		controller.heaterPin.Write(embd.Low)
+	}
+}
+
+func (controller *Controller) initPins() {
+	// Get the pin for the call for heat.
+	pin, err := embd.NewDigitalPin(controller.heaterPinNumber)
+	if err != nil {
+		panic(err)
+	}
+	pin.SetDirection(embd.Out)
+	controller.heaterPin = pin
+}
---
+++ go/controller/zone.go
@@ -0,0 +1,125 @@
+package controller
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kidoman/embd"
+	"github.com/vend/features/log"
+)
+
+// Zone represents a single zone that can optionally be turned on or off.
+type Zone struct {
+	Name           string        `json:"name"`
+	Active         bool          `json:"active"`
+	CallForHeatPin uint32        `json:"call_for_heat_pin"`
+	DamperOnPin    *uint32       `json:"damper_on_pin"`
+	DamperOffPin   *uint32       `json:"damper_off_pin"`
+	Interval       time.Duration `json:"-"`
+	callForHeatPin embd.DigitalPin
+	damperOnPin    embd.DigitalPin
+	damperOffPin   embd.DigitalPin
+}
+
+// NewZone returns an initialized zone.
+func NewZone(name string, pin uint32) *Zone {
+	return &Zone{
+		Name:           name,
+		CallForHeatPin: pin,
+		Interval:       time.Second,
+	}
+}
+
+func (zone *Zone) run(change chan<- bool) {
+	zone.initPins()
+	zone.UpdateDamperPins()
+
+	for {
+		reading, err := zone.callForHeatPin.Read()
+		if err != nil {
+			log.Error(fmt.Sprintf("Error reading call for heat pin %d in zone %s: %s", zone.CallForHeatPin, zone.Name, err))
+		} else {
+			// Toggle and set the damper pins if we've changed.
+			if reading == embd.High && !zone.Active {
+				zone.Active = true
+				zone.UpdateDamperPins()
+				change <- true
+			} else if reading == embd.Low && zone.Active {
+				zone.Active = false
+				zone.UpdateDamperPins()
+				change <- true
+			}
+		}
+		time.Sleep(zone.Interval)
+	}
+
+	/**
+	 * Possible alternate implementation
+	 */
+
+	// err := zone.callForHeatPin.Watch(embd.EdgeRising, func(pin embd.DigitalPin) {
+	// 	zone.Active = true
+	// 	zone.UpdateDamperPins()
+	// 	change <- true
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+	//
+	// err = zone.callForHeatPin.Watch(embd.EdgeFalling, func(pin embd.DigitalPin) {
+	// 	zone.Active = false
+	// 	zone.UpdateDamperPins()
+	// 	change <- true
+	// })
+	// if err != nil {
+	// 	panic(err)
+	// }
+}
+
+// UpdateDamperPins ensure the damper pins represent the current active state.
+func (zone *Zone) UpdateDamperPins() {
+	onTarget := embd.Low
+	offTarget := embd.High
+	if zone.Active {
+		onTarget = embd.High
+		offTarget = embd.Low
+	}
+
+	if zone.damperOnPin != nil {
+		zone.damperOnPin.Write(onTarget)
+	}
+
+	if zone.damperOffPin != nil {
+		zone.damperOffPin.Write(offTarget)
+	}
+}
+
+func (zone *Zone) initPins() {
+	// Get the pin for the call for heat.
+	pin, err := embd.NewDigitalPin(zone.CallForHeatPin)
+	if err != nil {
+		panic(err)
+	}
+	pin.SetDirection(embd.In)
+	zone.callForHeatPin = pin
+
+	// Set the damper on pin if required.
+	if zone.DamperOnPin != nil {
+		pin, err := embd.NewDigitalPin(zone.DamperOnPin)
+		if err != nil {
+			panic(err)
+		}
+		pin.SetDirection(embd.Out)
+		zone.damperOnPin = pin
+	}
+
+	// Set the damper on pin if required.
+	if zone.DamperOnPin != nil {
+		pin, err := embd.NewDigitalPin(zone.DamperOffPin)
+		if err != nil {
+			panic(err)
+		}
+		pin.SetDirection(embd.Out)
+		zone.damperOffPin = pin
+	}
+}
